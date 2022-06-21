d3b004465e3ab90764fa996f9154ee1bc35671b2
Amirerfan Rafati <aerfanr@mailo.com>
2022-06-21 16:26:56 +0430

    Added options window

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
 use bevy::ecs::schedule::ShouldRun;
 use bevy::prelude::*;
 use bevy::sprite::collide_aabb::collide;
-use bevy_egui::{egui, EguiPlugin, EguiContext};
-use egui::plot::{Plot, Bar, BarChart, Line, Value, Values};
+use bevy_egui::{egui, EguiContext, EguiPlugin};
+use egui::plot::{Bar, BarChart, Line, Plot, Value, Values};
+use egui::widgets::DragValue;
 use rand::Rng;
 
-const SIMULATION_SPEED: f32 = 6.;
 const MOVEMENT_SPEED: f32 = 150.;
 const DAY_LENGTH: f32 = 10.;
 const NIGHT_LENGTH: f32 = 2.;
-const BASE_ENERGY_COST: f32 = 1. / (NIGHT_LENGTH + DAY_LENGTH) / MOVEMENT_SPEED;
-const BASE_ENERGY: f32 = 1.;
-const TRAIT_CHANGE_INTENSITY: f32 = 0.1;
-const PERSON_COUNT: i32 = 10;
-const FOOD_COUNT: i32 = 100;
 
 struct Sunset(bool);
 struct DayTimer(Timer);
@@ -22,10 +17,36 @@ struct NightTimer(Timer);
 struct Charts {
     population: Vec<Bar>,
     food_count: Vec<Bar>,
-    avg_speed: Vec<Value>
+    avg_speed: Vec<Value>,
+}
+struct Started(bool);
+struct Options {
+    simulation_speed: f32,
+    movement_speed: f32,
+    day_length: f32,
+    night_length: f32,
+    base_energy_cost: f32,
+    base_energy: f32,
+    trait_change_intensity: f32,
+    person_count: i32,
+    food_count: i32,
 }
 
-struct Started(bool);
+impl Default for Options {
+    fn default() -> Self {
+        Self {
+            simulation_speed: 1.,
+            movement_speed: MOVEMENT_SPEED,
+            day_length: DAY_LENGTH,
+            night_length: NIGHT_LENGTH,
+            base_energy_cost: 1. / (NIGHT_LENGTH + DAY_LENGTH) / MOVEMENT_SPEED,
+            base_energy: 1.,
+            trait_change_intensity: 0.1,
+            person_count: 10,
+            food_count: 100,
+        }
+    }
+}
 
 struct RandomizeDirections;
 struct SpawnFood;
@@ -51,9 +72,9 @@ struct Traits {
 }
 
 impl Traits {
-    fn variation(&self) -> Traits {
+    fn variation(&self, change_intensity: f32) -> Traits {
         Traits {
-            speed: self.speed + (rand::random::<f32>() * 2. - 1.) * TRAIT_CHANGE_INTENSITY,
+            speed: self.speed + (rand::random::<f32>() * 2. - 1.) * change_intensity,
         }
     }
 }
@@ -72,7 +93,7 @@ fn bar_options() -> Bar {
         base_offset: None,
         fill: egui::Color32::LIGHT_GRAY,
         orientation: egui::widgets::plot::Orientation::Vertical,
-        stroke: egui::Stroke::none()
+        stroke: egui::Stroke::none(),
     }
 }
 
@@ -86,9 +107,7 @@ fn get_random_location(window: &Window) -> Transform {
     Transform::from_xyz(x, y, 0.)
 }
 
-fn setup(
-    mut commands: Commands,
-) {
+fn setup(mut commands: Commands) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 }
 
@@ -101,9 +120,12 @@ fn start_simulation(
     started: Res<Started>,
     mut day_timer: ResMut<DayTimer>,
     mut night_timer: ResMut<NightTimer>,
+    options: Res<Options>,
 ) {
-    if !started.is_changed() || !started.0 { return };
-    for _i in 0..PERSON_COUNT {
+    if !started.is_changed() || !started.0 {
+        return;
+    };
+    for _i in 0..options.person_count {
         commands
             .spawn_bundle(SpriteBundle {
                 texture: asset_server.load("person1.png"),
@@ -112,15 +134,18 @@ fn start_simulation(
             })
             .insert(Person)
             .insert(Hungry)
-            .insert(Energy(BASE_ENERGY))
+            .insert(Energy(options.base_energy))
             .insert(Traits { speed: 1. });
     }
     ev_randomize.send(RandomizeDirections);
 
     ev_spawn_food.send(SpawnFood);
 
-    day_timer.0.reset();
-    night_timer.0.reset();
+    day_timer.0 = Timer::from_seconds(options.day_length / options.simulation_speed, true);
+    night_timer.0 = Timer::from_seconds(
+        (options.day_length + options.night_length) / options.simulation_speed,
+        true,
+    );
 }
 
 fn spawn_food(
@@ -128,9 +153,10 @@ fn spawn_food(
     asset_server: Res<AssetServer>,
     windows: Res<Windows>,
     mut events: EventReader<SpawnFood>,
+    options: Res<Options>,
 ) {
     for _event in events.iter() {
-        for _i in 0..FOOD_COUNT {
+        for _i in 0..options.food_count {
             commands
                 .spawn_bundle(SpriteBundle {
                     texture: asset_server.load("food1.png"),
@@ -150,19 +176,24 @@ fn random_movement(
         (With<Person>, Without<Returning>, Without<Dead>),
     >,
     windows: Res<Windows>,
+    options: Res<Options>,
 ) {
     let mut rng = rand::thread_rng();
 
     for mut sprite in sprite.iter_mut() {
-        let rotation_delta =
-            Quat::from_rotation_z((rng.gen::<f32>() - 0.5) * 12. * time.delta_seconds() * SIMULATION_SPEED);
+        let rotation_delta = Quat::from_rotation_z(
+            (rng.gen::<f32>() - 0.5) * 12. * time.delta_seconds() * options.simulation_speed,
+        );
         sprite.0.rotation *= rotation_delta;
 
         let rotation_rad = sprite.0.rotation.to_euler(EulerRot::ZYX).0;
-        let distance = MOVEMENT_SPEED * sprite.1.speed * time.delta_seconds() * SIMULATION_SPEED;
+        let distance = options.movement_speed
+            * sprite.1.speed
+            * time.delta_seconds()
+            * options.simulation_speed;
         let delta_x = distance * rotation_rad.cos();
         let delta_y = distance * rotation_rad.sin();
-        let e = distance * sprite.1.speed * BASE_ENERGY_COST;
+        let e = distance * sprite.1.speed * options.base_energy_cost;
 
         sprite.2 .0 -= e;
 
@@ -196,14 +227,18 @@ fn home_movement(
         (With<Person>, With<Returning>, Without<Dead>),
     >,
     windows: Res<Windows>,
+    options: Res<Options>,
 ) {
     let window = windows.primary();
     let width = window.width() / 2.;
     let height = window.height() / 2.;
 
     for mut sprite in sprites.iter_mut() {
-        let d = MOVEMENT_SPEED * time.delta_seconds() * SIMULATION_SPEED * sprite.2.speed;
-        let e = d * sprite.2.speed * BASE_ENERGY_COST;
+        let d = options.movement_speed
+            * time.delta_seconds()
+            * options.simulation_speed
+            * sprite.2.speed;
+        let e = d * sprite.2.speed * options.base_energy_cost;
 
         let mut transform = sprite.0;
         let left = transform.translation.x + width;
@@ -309,6 +344,7 @@ fn night_timer(
     mut ev_randomize: EventWriter<RandomizeDirections>,
     mut ev_spawn_food: EventWriter<SpawnFood>,
     mut ev_reproduce: EventWriter<Reproduce>,
+    options: Res<Options>,
 ) {
     timer.0.tick(time.delta());
     if timer.0.finished() {
@@ -318,14 +354,17 @@ fn night_timer(
             commands.entity(person).despawn();
         }
         for mut person in to_live.iter_mut() {
-            person.1 .0 = BASE_ENERGY;
+            person.1 .0 = options.base_energy;
             commands.entity(person.0).insert(Hungry);
             commands
                 .entity(person.0)
                 .remove_bundle::<(Fertile, Returning, AtHome)>();
         }
         for person in to_reproduce.iter() {
-            ev_reproduce.send(Reproduce(*person.0, person.1.variation()));
+            ev_reproduce.send(Reproduce(
+                *person.0,
+                person.1.variation(options.trait_change_intensity),
+            ));
         }
         ev_randomize.send(RandomizeDirections);
         ev_spawn_food.send(SpawnFood);
@@ -336,6 +375,7 @@ fn reproduce(
     mut commands: Commands,
     mut events: EventReader<Reproduce>,
     asset_server: Res<AssetServer>,
+    options: Res<Options>,
 ) {
     for event in events.iter() {
         commands
@@ -346,7 +386,7 @@ fn reproduce(
             })
             .insert(Person)
             .insert(Hungry)
-            .insert(Energy(BASE_ENERGY))
+            .insert(Energy(options.base_energy))
             .insert(event.1);
     }
 }
@@ -357,7 +397,8 @@ fn count_stuff(
     foods: Query<&Food>,
     time: Res<Time>,
     mut charts: ResMut<Charts>,
-    mut exit: EventWriter<bevy::app::AppExit>
+    mut exit: EventWriter<bevy::app::AppExit>,
+    options: Res<Options>,
 ) {
     for _event in events.iter() {
         let mut speed_avg = 0.;
@@ -376,13 +417,13 @@ fn count_stuff(
         println!("{};\t{};\t{};", people_count, food_count, speed_avg);
 
         charts.population.push(Bar {
-            argument: time.seconds_since_startup() * SIMULATION_SPEED as f64,
+            argument: time.seconds_since_startup() * options.simulation_speed as f64,
             value: people_count as f64,
             name: String::from("Population"),
             ..bar_options()
         });
         charts.food_count.push(Bar {
-            argument: time.seconds_since_startup() * SIMULATION_SPEED as f64,
+            argument: time.seconds_since_startup() * options.simulation_speed as f64,
             value: food_count as f64,
             name: String::from("Food Count"),
             fill: egui::Color32::RED,
@@ -390,41 +431,70 @@ fn count_stuff(
             ..bar_options()
         });
         charts.avg_speed.push(Value {
-            x: time.seconds_since_startup() * SIMULATION_SPEED as f64,
-            y: speed_avg as f64
+            x: time.seconds_since_startup() * options.simulation_speed as f64,
+            y: speed_avg as f64,
         });
     }
 }
 
-fn plot_stuff(
-    mut context: ResMut<EguiContext>,
-    charts: Res<Charts>
-) {
+fn plot_stuff(mut context: ResMut<EguiContext>, charts: Res<Charts>) {
     egui::Window::new("Stats").show(context.ctx_mut(), |ui| {
         let population_chart = BarChart::new(charts.population.clone());
         let food_chart = BarChart::new(charts.food_count.clone());
         let avg_speed_line = Line::new(Values::from_values(charts.avg_speed.clone()));
-        Plot::new("Stats_1").height(200.)
-            .show(ui, |plot_ui| {
-                plot_ui.bar_chart(population_chart);
-                plot_ui.bar_chart(food_chart);
-            });
-        Plot::new("Stats_2").height(200.)
-            .show(ui, |plot_ui| {
-                plot_ui.line(avg_speed_line)
-            });
+        Plot::new("Stats_1").height(200.).show(ui, |plot_ui| {
+            plot_ui.bar_chart(population_chart);
+            plot_ui.bar_chart(food_chart);
+        });
+        Plot::new("Stats_2")
+            .height(200.)
+            .show(ui, |plot_ui| plot_ui.line(avg_speed_line));
     });
 }
 
-fn options(
+fn options_window(
     mut context: ResMut<EguiContext>,
-    mut started: ResMut<Started>
+    mut started: ResMut<Started>,
+    mut options: ResMut<Options>,
 ) {
-    egui::Window::new("Options").show(context.ctx_mut(), |ui| {
-        if ui.button("Start Simulation").clicked() {
-            started.0 = true;
-        }
-    });
+    egui::Window::new("Options")
+        .enabled(!started.0)
+        .show(context.ctx_mut(), |ui| {
+            ui.label("Simulation speed:");
+            ui.add(
+                DragValue::new(&mut options.simulation_speed)
+                    .clamp_range(0.1..=100.)
+                    .speed(0.2),
+            );
+
+            ui.label("Movement speed:");
+            ui.add(DragValue::new(&mut options.movement_speed).speed(5));
+
+            ui.label("Day length:");
+            ui.add(DragValue::new(&mut options.day_length).clamp_range(0.1..=f64::MAX));
+
+            ui.label("Night length:");
+            ui.add(DragValue::new(&mut options.night_length).clamp_range(0.1..=f64::MAX));
+
+            ui.label("Energy cost:");
+            ui.add(DragValue::new(&mut options.base_energy_cost).speed(0.0001));
+
+            ui.label("Base energy:");
+            ui.add(DragValue::new(&mut options.base_energy));
+
+            ui.label("Trait change intensity:");
+            ui.add(DragValue::new(&mut options.trait_change_intensity).speed(0.05));
+
+            ui.label("Base population:");
+            ui.add(DragValue::new(&mut options.person_count).speed(10));
+
+            ui.label("Food count:");
+            ui.add(DragValue::new(&mut options.food_count).speed(10));
+
+            if ui.button("Start Simulation").clicked() {
+                started.0 = true;
+            }
+        });
 }
 
 fn background_color(sunset: Res<Sunset>, mut clear_color: ResMut<ClearColor>) {
@@ -472,8 +542,11 @@ fn run_if_day(sunset: Res<Sunset>) -> ShouldRun {
 }
 
 fn run_if_started(started: Res<Started>) -> ShouldRun {
-    if started.0 && !started.is_changed() { ShouldRun::Yes }
-    else { ShouldRun::No }
+    if started.0 && !started.is_changed() {
+        ShouldRun::Yes
+    } else {
+        ShouldRun::No
+    }
 }
 
 //fn debug1(query: Query<Entity, Without<Dead>>) {
@@ -483,16 +556,22 @@ fn run_if_started(started: Res<Started>) -> ShouldRun {
 //}
 
 fn main() {
+    let options = Options::default();
+
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.9, 0.8, 0.8)))
         .insert_resource(Sunset(false))
         .insert_resource(Charts::default())
         .insert_resource(Started(false))
+        .insert_resource(Options::default())
         .insert_resource(DayTimer {
-            0: Timer::from_seconds(DAY_LENGTH / SIMULATION_SPEED, true),
+            0: Timer::from_seconds(options.day_length / options.simulation_speed, true),
         })
         .insert_resource(NightTimer {
-            0: Timer::from_seconds((NIGHT_LENGTH + DAY_LENGTH) / SIMULATION_SPEED, true),
+            0: Timer::from_seconds(
+                (options.night_length + options.day_length) / options.simulation_speed,
+                true,
+            ),
         })
         .add_plugins(DefaultPlugins)
         .add_plugin(EguiPlugin)
@@ -511,17 +590,17 @@ fn main() {
                 .with_system(random_movement)
                 .with_system(collision),
         )
-        .add_system_set_to_stage(CoreStage::PreUpdate,
+        .add_system_set_to_stage(
+            CoreStage::PreUpdate,
             SystemSet::new()
                 .with_run_criteria(run_if_started)
-                .with_system(night_timer)
-
+                .with_system(night_timer),
         )
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(run_if_started)
                 .with_system(day_timer)
-                .with_system(count_stuff)
+                .with_system(count_stuff),
         )
         .add_system(fertile_return)
         .add_system(randomize_directions)
@@ -529,7 +608,7 @@ fn main() {
         .add_system(reproduce)
         .add_system(energy)
         .add_system(plot_stuff)
-        .add_system(options)
+        .add_system(options_window)
         .add_event::<RandomizeDirections>()
         .add_event::<SpawnFood>()
         .add_event::<Reproduce>()
